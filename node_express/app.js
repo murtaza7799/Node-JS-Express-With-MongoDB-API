b8cd20d1d4c710594038cf84f1f47d000f29ea31
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2021-12-17 01:07:01 +0500

    master

--- node_express/app.js
+++ node_express/app.js
@@ -1,24 +1,31 @@
+// App Controller
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+// middleware that use  to log Http req  and errors
 var logger = require('morgan');
-
+// assign routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+// use express
 var app = express();
 
-// view engine setup
+// view engine setup 
+// here we use ejs view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+// log req in console 
 app.use(logger('dev'));
+// a method inbuilt in express to recognize the incoming Request Object as a JSON Object.
+//we NEED express.json() and express.urlencoded() for POST and PUT request and not for Delete and get request
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+//for routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
