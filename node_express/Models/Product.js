c79e0007dfb8efe8d3ef2be97b49026932af0011
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2021-12-20 21:21:04 +0500

    master

--- node_express/Models/Product.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const productSchema = mongoose.Schema({
-  name: String
-});
-const Product = mongoose.model("Product", productSchema);
-module.exports = Product;
