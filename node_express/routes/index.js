7a25dfbb2bcf8082266a68a1ad3e4ca07793f67d
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2021-12-20 21:08:33 +0500

    master

--- node_express/routes/index.js
+++ node_express/routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
 
+var express = require('express');
+var Product = require("../models/Product");
+var router = express.Router();
+const mongoose = require('mongoose');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Test' });
 });
+
+const silence = new Product({ name: 'Silence' });
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/sp19-bse').then(() =>console.log("Connected")).catch(err => console.log("Could not connect to Mongo"));
+  try {
+    await silence.save();
+    
+  } catch (error) {
+    response.status(500).send(error);
+  }
+
+}
+main().catch(err => console.log(err));
+ 
 
 module.exports = router;
